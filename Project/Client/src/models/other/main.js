e8cd043e0c0b102efe77fe02a38cbed233c985a3
Ramy Bachayev <90751345+ramyb3@users.noreply.github.com>
2022-02-08 19:08:53 +0200

    Update main.js

--- Project/Client/src/models/other/main.js
+++ Project/Client/src/models/other/main.js
@@ -9,24 +9,24 @@ function Main(props)
 
     return(<div >
 
-        <big><b> Connected User: {props.props.name}  </b></big><br/><br/>
+        <big><b style={{color: 'rgb(69, 78, 208)'}}> Connected User: {props.props.name} </b></big><br/><br/>
  
         {props.props.perm.includes("View Movies") ?
 
-            <Link to="movies"><input type="button" value="Movies" style={{cursor: "pointer"}}/></Link> : null
+            <Link to="movies"><input type="button" value="Movies" className="button"/></Link> : null
         }&nbsp;
 
         {props.props.perm.includes("View Subscriptions") ?  
             
-            <Link to="subscriptions"><input type="button" value="Subscriptions" style={{cursor: "pointer"}}/></Link> : null
+            <Link to="subscriptions"><input type="button" value="Subscriptions" className="button"/></Link> : null
         }&nbsp;
 
         {props.props.name=="admin" ? 
 
-            <Link to="manageUsers"><input type="button" value="Users Management" style={{cursor: "pointer"}}/></Link> : null
+            <Link to="manageUsers"><input type="button" value="Users Management" className="button"/></Link> : null
         }&nbsp;
 
-        <Link to="/"><input type="button" value="Logout" onClick={reset} style={{cursor: "pointer"}}/></Link> <br/>
+        <Link to="/"><input type="button" value="Logout" onClick={reset} className="button"/></Link> <br/>
         
         <Outlet />
 
