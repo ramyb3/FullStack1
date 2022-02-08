4dfaa782c0cdc95a8e42034da5b09d6cb97416a5
Ramy Bachayev <90751345+ramyb3@users.noreply.github.com>
2022-02-08 19:20:37 +0200

    Update addMovie.js

--- Project/Client/src/models/movies/addMovie.js
+++ Project/Client/src/models/movies/addMovie.js
@@ -46,45 +46,49 @@ function AddMovie(props)
         setMovie({...movie, genres: value});
     }
 
-    return(<div>
+    return(<div style={{textAlign: 'center'}}>
 
-        <h2>Add Movie Page</h2>
+        <div className="box">
 
-        Enter the name of the movie: <input type="text" onChange={e=> setMovie({...movie, name: e.target.value})}/><br/><br/>
+            <h2>Add Movie Page</h2>
 
-        Select the genres of the movie: <br/>
-        <select multiple="multiple" onChange={(e)=> check(e)}>
-            <option value="Action">Action</option>
-            <option value="Adventure">Adventure </option>
-            <option value="Anime">Anime </option>
-            <option value="Comedy">Comedy </option>
-            <option value="Crime">Crime </option>
-            <option value="Drama">Drama</option>
-            <option value="Espionage">Espionage </option>
-            <option value="Family">Family </option>
-            <option value="Fantasy">Fantasy </option>
-            <option value="History">History </option>
-            <option value="Horror">Horror </option>
-            <option value="Legal">Legal </option>
-            <option value="Medical">Medical </option>
-            <option value="Music">Music </option>
-            <option value="Mystery">Mystery </option>
-            <option value="Romance">Romance </option>
-            <option value="Science-Fiction">Science-Fiction</option>
-            <option value="Sports">Sports</option>
-            <option value="Supernatural">Supernatural </option>
-            <option value="Thriller">Thriller</option>
-            <option value="War">War</option>    
-            <option value="Western">Western</option>    
-        </select><br/><br/>
+            <input placeholder="Enter Movie Name" type="text" onChange={e=> setMovie({...movie, name: e.target.value})}/><br/><br/>
 
-        Enter the movie image link: <input type="url" onChange={e=> setMovie({...movie, image: e.target.value})}/><br/><br/>
+            Select the genres of the movie: <br/>
+            <select style={{width: '200px', textAlign: 'center'}} multiple="multiple" onChange={(e)=> check(e)}>
+                <option value="Action">Action</option>
+                <option value="Adventure">Adventure </option>
+                <option value="Anime">Anime </option>
+                <option value="Comedy">Comedy </option>
+                <option value="Crime">Crime </option>
+                <option value="Drama">Drama</option>
+                <option value="Espionage">Espionage </option>
+                <option value="Family">Family </option>
+                <option value="Fantasy">Fantasy </option>
+                <option value="History">History </option>
+                <option value="Horror">Horror </option>
+                <option value="Legal">Legal </option>
+                <option value="Medical">Medical </option>
+                <option value="Music">Music </option>
+                <option value="Mystery">Mystery </option>
+                <option value="Romance">Romance </option>
+                <option value="Science-Fiction">Science-Fiction</option>
+                <option value="Sports">Sports</option>
+                <option value="Supernatural">Supernatural </option>
+                <option value="Thriller">Thriller</option>
+                <option value="War">War</option>    
+                <option value="Western">Western</option>    
+            </select><br/><br/>
 
-        Enter the date the movie premiered: <input type="date" onChange={e=> setMovie({...movie, date: e.target.value})}/><br/><br/>
+            <input style={{width: "80%"}} placeholder="Enter Movie Image Link" type="url" onChange={e=> setMovie({...movie, image: e.target.value})}/><br/><br/>
 
-        <input type="button" value="Save" onClick={()=> send(1)} style={{cursor: "pointer"}}/> 
-        <input type="button" value="Cancel" onClick={()=> send(2)} style={{cursor: "pointer"}}/><br/><br/>
+            Enter the date the movie premiered:<br/> <input type="date" onChange={e=> setMovie({...movie, date: e.target.value})}/><br/><br/>
+
+            <input type="button" value="Save" onClick={()=> send(1)} className="button"/> 
+            <input type="button" value="Cancel" onClick={()=> send(2)} className="button"/><br/><br/>
         
+        </div><br/>
+
     </div>)
 }
 
